00c198909aa3e088c69c113793ba34d7fd0f0754
prerreslin <sanyatop011209@gmail.com>
2025-02-13 17:56:29 +0200

    main route

--- dz/dz/urls.py
+++ dz/dz/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from dzz.views import my_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+urlpatterns = [
+    path('', my_view, name='home'),
 ]
--- dz/dzz/views.py
+++ dz/dzz/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
+def my_view(request):
+    return render(request, 'main.html', {'message': 'Привіт, Django!'})
